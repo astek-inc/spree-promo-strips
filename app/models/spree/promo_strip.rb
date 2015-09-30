42805a231c3243354f89173facce8f7c62a0573f
Edwin Horneij <edwin@astekwallcovering.com>
2015-09-30 10:16:58 -0700

    Removing redundant autosave setting

--- app/models/spree/promo_strip.rb
+++ app/models/spree/promo_strip.rb
@@ -4,7 +4,7 @@ module Spree
     self.table_name = 'promo_strips'
 
     belongs_to :promo_strip_layout
-    has_many :promo_strip_items, autosave: :true
+    has_many :promo_strip_items
 
     validates_presence_of :promo_strip_layout_id #, :status, :default
 

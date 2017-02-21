720a048595f3d6eb23717f37a09259adec1c3305
Edwin Horneij <edwin@astekwallcovering.com>
2017-02-21 14:43:12 -0800

    Update original for override

--- app/overrides/add_promo_strips_to_admin_menu.rb
+++ app/overrides/add_promo_strips_to_admin_menu.rb
@@ -1,9 +1,9 @@
 Deface::Override.new(
-    :original => 'cde45f1821e0d32ccb66035f6533f96c53acbb32',
-    :virtual_path => 'spree/layouts/admin',
-    :name => 'add_promo_strips_item_to_admin_menu',
-    :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-    :text => "<ul class=\"nav nav-sidebar\">
-                <%= tab t(:promo_strips), :url => admin_promo_strips_path, :icon => 'star' %>
-              </ul>"
+    original: '8cf26bbac9fde4487b558964abde5a046a59a62d',
+    virtual_path: 'spree/layouts/admin',
+    name: 'add_promo_strips_item_to_admin_menu',
+    insert_bottom: "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+    text: "<ul class=\"nav nav-sidebar\">
+        <%= tab t(:promo_strips), url: admin_promo_strips_path, icon: 'star' %>
+      </ul>"
 )
